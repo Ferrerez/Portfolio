4ef9b1c6aafb227c118a4f71c0a9e4052a07d8c8
Ferrerez <rodolfomocmg@gmail.com>
2022-11-05 16:33:29 -0300

    ,
    ,
    
    Co-Authored-By: Rodolfo Ferrerez Rocha <117477037+Ferrerez@users.noreply.github.com>

--- Projeto 1/script.js
+++ Projeto 1/script.js
@@ -1,6 +1,6 @@
-    var bo1 = document.getElementById('b1')
-    var bo2 = document.getElementById('b2')
-    var bo3 = document.getElementById('b3')
+var bo1 = document.getElementById('b1')
+var bo2 = document.getElementById('b2')
+var bo3 = document.getElementById('b3')
     
     function verificar1 () {
         alert('Você clicou no 1º botão!')
---
+++ Projeto 2/script.js
@@ -0,0 +1,5 @@
+function verificar () {
+    var nome = prompt("Qual seu nome?")
+    var idade = prompt(`Olá, ${nome}! Quantos nos você tem?` )
+    alert (`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}
