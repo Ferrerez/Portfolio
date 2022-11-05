bb6cdbf26cb8930d4f3254885978caae103796e8
Ferrerez <rodolfomocmg@gmail.com>
2022-11-05 18:02:56 -0300

    ,
    ,
    
    Co-Authored-By: Rodolfo Ferrerez Rocha <117477037+Ferrerez@users.noreply.github.com>

---
+++ Projeto 3/script.js
@@ -0,0 +1,6 @@
+function verificar () {
+    let n = prompt('Digite um número qualquer:')
+    let n2 = Number (n)
+    
+    alert(`Antes de ${n2}, temos o número ${n2-1}.\nDepois de ${n2}, temos o número ${n2+1}.`)
+}
