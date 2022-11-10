5d7ebe89d099cb824529e334b8cf10c2669ed429
Rodolfo Ferrerez Rocha <rodolfomocmg@gmail.com>
2022-11-10 19:58:12 -0300

    .

--- 7 - Conversor de moedas/script.js
+++ 7 - Conversor de moedas/script.js
@@ -1,9 +1,9 @@
 var d = prompt ('Antes de mais nada. Quanto está a cotação do doólar agora?')
-
+var r = prompt ('Quantos R$ você tem na carteira?')
     function converter() {
-        var r = prompt ('Quantos R$ você tem na carteira?')
-        var res = Number (r*d)
-            res.innerHTML = (`De acordo com a cotação do dia, você comprará ${res} em dolares.`)
+        
+        var re = Number (r*d)
+            res.innerHTML = (`De acordo com a cotação do dia, você comprará US$ ${re} dolares.`)
         
 
         
