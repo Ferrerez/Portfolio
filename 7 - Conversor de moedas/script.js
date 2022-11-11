7cde110a44bfa98855c14208a68480c83be91578
Rodolfo Ferrerez Rocha <rodolfomocmg@gmail.com>
2022-11-11 16:58:57 -0300

    .

--- 7 - Conversor de moedas/script.js
+++ 7 - Conversor de moedas/script.js
@@ -1,11 +1,17 @@
 var d = prompt ('Antes de mais nada. Quanto está a cotação do dólar agora?')
+if(d <= 0) {
+    alert('Não é possivel converter um valor zerado ou negativo!')
+} else {
 var r = prompt ('Quantos R$ você tem na carteira?')
+}
+if(r <= 0) {
+    alert('Não é possivel converter um valor zerado ou negativo!')
+} else {
+
     function converter() {
         
-        var re = Number (r/d)
-            res.innerHTML = (`De acordo com a cotação do dia, você comprará US$ ${re} dolares.`)
-        
-
-        
+        var re = parseFloat (r/d).toFixed(2).replace('.', ',')
+            res.innerHTML = (`<strong>De acordo com a cotação do dia, você comprará US$ ${re} dolares.</strong>`)
     }
+}
 
