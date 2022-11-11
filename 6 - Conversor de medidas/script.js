3f3b1bfd7958955caf5639f6605ca3b287db79e5
Rodolfo Ferrerez Rocha <rodolfomocmg@gmail.com>
2022-11-11 19:20:11 -0300

    .

--- 6 - Conversor de medidas/script.js
+++ 6 - Conversor de medidas/script.js
@@ -1,17 +1,17 @@
-function verificar () {
-    var m = parseFloat(prompt(`Digite um distância em metros (m): `))
-    m.toFixed(2).replace('.', ',')
-    if(m <= 0) {
-        alert('Não é possivel converter um valor zerado ou negativo!')
-    } else {
-        res.innerHTML = (`A distância de ${m} metros, corresponde a...<br><br>
-        ${m/1000} quilômetros (Km)<br>
-        ${m/100} hectômetros (Hm)<br>
-        ${m/10} decâmetros (Dam)<br>
-        ${m*10} decímetros (dm)<br>
-        ${m*100} centímetros (cm)<br>
-        ${m*1000} milímetros (mm)`)
-        
+var j = prompt(`Digite um distância em metros (m): `)
+if(j <= 0) {
+    alert('Não é possivel converter um valor zerado ou negativo!')
+} else {
+    function converter () {
+        var m = parseFloat (j).toFixed(2).replace('.', ',')
+            res.innerHTML = (`<strong>A distância de ${m} metros, corresponde a...<br><br>
+            ${m/1000} quilômetros (Km)<br>
+            ${m/100} hectômetros (Hm)<br>
+            ${m/10} decâmetros (Dam)<br>
+            ${m*10} decímetros (dm)<br>
+            ${m*100} centímetros (cm)<br>
+            ${m*1000} milímetros (mm)</strong>`)
     }
-    
+        
 }
+    
--- 7 - Conversor de moedas/script.js
+++ 7 - Conversor de moedas/script.js
@@ -3,15 +3,11 @@ if(d <= 0) {
     alert('Não é possivel converter um valor zerado ou negativo!')
 } else {
 var r = prompt ('Quantos R$ você tem na carteira?')
-}
-if(r <= 0) {
+} if(r <= 0) {
     alert('Não é possivel converter um valor zerado ou negativo!')
 } else {
-
     function converter() {
-        
         var re = parseFloat (r/d).toFixed(2).replace('.', ',')
             res.innerHTML = (`<strong>De acordo com a cotação do dia, você comprará US$ ${re} dolares.</strong>`)
     }
 }
-
