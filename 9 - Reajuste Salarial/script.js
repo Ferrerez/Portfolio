9394caafc2380615167288ea3b1120188113eb62
Rodolfo Ferrerez Rocha <rodolfomocmg@gmail.com>
2022-11-12 17:56:37 -0300

    .

---
+++ 9 - Reajuste Salarial/script.js
@@ -0,0 +1,6 @@
+function reajustar () {
+    var nome = prompt('Qual o nome do funcionário?')
+    var salario = prompt(`Qual é o salário de ${nome}?`)
+    var reajus = prompt(`O salário de ${nome} vai ser ajustado em qual porcentagem?`)
+
+}
